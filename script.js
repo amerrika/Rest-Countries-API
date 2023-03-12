c098d322a03735aeb93e3e5dc9e955a7b6c871b6
Amerrika <amer.tricic@live.com>
2023-03-12 11:29:56 +0100

    Country Details Page

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const countriesContainer = document.querySelector(".countries");
 const inputField = document.getElementById("search-bar");
 const btnRegions = document.getElementById("btnRegions");
-const heading = document.querySelector(".primary-heading");
+const primaryHeading = document.querySelector(".primary-heading");
 const regions = document.querySelectorAll(".filter-options p");
+const sectionSearch = document.querySelector(".section-search");
+const countryDetailsPage = document.querySelector(".country-details");
 
 const renderCountry = function(data) { // data represents an array of country-objects that are fetched
     countriesContainer.innerHTML = '';
@@ -11,6 +13,9 @@ const renderCountry = function(data) { // data represents an array of country-ob
     data.forEach(function(country){
         const article = document.createElement("article");
         article.classList.add("country");
+        article.setAttribute('data-code', `${country.alpha3Code}`);
+        
+        
 
         article.innerHTML = `
             <img class="country-img" src="${country.flag}" alt="">
@@ -32,10 +37,11 @@ const renderCountry = function(data) { // data represents an array of country-ob
 const getData = async function(url){
     try {
         const res = await fetch(url);
-        const data = await res.json();
+        const data = await res.json(); // data represents an array of country-objects that are fetched
 
+        console.log(data)
         renderCountry(data);
-        return data;
+        return data; 
 
     } catch(err) {
         console.log(err)
@@ -54,7 +60,7 @@ const initialPageLoad = async function() {
 
 initialPageLoad();
 
-heading.addEventListener("click", initialPageLoad);
+primaryHeading.addEventListener("click", initialPageLoad);
 
 // Search Countries: Input Event
 
@@ -69,11 +75,9 @@ inputField.addEventListener('input', function(e){
     }
 })
 
-// Region Options Toggle: Click Event
+// Filter Regions Feature: Click Event
 
-btnRegions.addEventListener("click", toggleRegions)
-
-// Filter Regions
+btnRegions.addEventListener("click", toggleRegions);
 
 regions.forEach(region => {
     region.addEventListener("click", () => {
@@ -85,4 +89,53 @@ regions.forEach(region => {
         
         getData(url);
     })
+})
+
+// Country Details Page Features 
+
+countriesContainer.addEventListener("click", function(e){
+    if(e.target.classList.contains("country-img")){
+        const code = e.target.parentElement.getAttribute('data-code');
+        console.log(code);
+        
+        // Fetch Country Object
+        (async () => {
+            const res = await fetch(`https://restcountries.com/v3.1/alpha/${code}`)
+            const data = await res.json();
+
+            // Display Country Details Page
+            countriesContainer.classList.add("display-none");
+            sectionSearch.classList.add("display-none");
+            countryDetailsPage.classList.remove("display-none");
+
+            // Create HTML
+            console.log(data[0].flags.png)
+            const html = `
+                <div class="country-details__img">
+                    <img id="country-details-img" src="${data[0].flags.png}" alt="">
+                </div>
+                <div class="country-details__content">
+                    <h2 class="country-title">${data[0].name.common}</h2>
+                    <div class="country-details__data">
+                        <div>
+                            <p><span>Native Name:</span> ${data[0].name.nativeName.sqi.official}</p>
+                            <p><span>Population:</span> 11,319,511</p>
+                            <p><span>Region:</span> Europe</p>
+                            <p><span>Subregion:</span> Western Europe</p>
+                            <p><span>Capital:</span> Brussels</p>
+                        </div>
+                        <div>
+                            <p><span>Top Level Domain:</span> .be</p>
+                            <p><span>Currencies:</span> Euro</p>
+                            <p><span>Languages:</span> Dutch, French, German</p>
+                        </div>
+                    </div>
+                    <div class="border-countries">
+                        <p><span>Border Countries:</span> France, Germany, Netherlands</p>
+                    </div>
+                </div>       
+            `
+            document.querySelector(".country-details").insertAdjacentHTML("beforeend", html);
+            })();
+    }
 })
